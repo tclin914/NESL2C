25868b12638269bf3693be4b1d759a0d9effa17e
jerrychen210054 <jerrychen210054@gmail.com>
2017-02-19 02:39:55 +0800

    [jerry] can generate sequential quicksort.c     not sure if it works. need to implement nesl.h

--- symtab.c
+++ symtab.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #define help(s) {printf("\thelp: %s\n",s);}
 
+void codegen(FILE *fptr, struct nodeType* node);
 
 struct SymTable *rootTable;
 
@@ -770,6 +771,8 @@ void typeAnalysis( struct nodeType *node){
           child = child->rsibling;
         }while(child!=node->child);
       }
+      node->counts = count;
+
       for(int i=0;i<count-1;i++){
         assert(child->valueType);
         assert(child->valueType == child->rsibling->valueType);
@@ -1179,22 +1182,22 @@ void dumpTable(FILE *fptr, struct nodeType* node){
           fprintf(fptr, "char* %s = malloc(sizeof(char*100));\n",table->entries[i].name);
           break;
         case TypeSEQ_I:
-          fprintf(fptr, "struct TypeSEQ_I *%s;\n", table->entries[i].name);
+          fprintf(fptr, "struct Sequence %s;\n", table->entries[i].name);
           break;
         case TypeSEQ_F:
-          fprintf(fptr, "struct TypeSEQ_F *%s;\n", table->entries[i].name);
+          fprintf(fptr, "struct Sequence %s;\n", table->entries[i].name);
           break;
         case TypeSEQ_B:
-          fprintf(fptr, "struct TypeSEQ_B *%s;\n", table->entries[i].name);
+          fprintf(fptr, "struct Sequence %s;\n", table->entries[i].name);
           break;
         case TypeSEQ_C:
-          fprintf(fptr, "struct TypeSEQ_C *%s;\n", table->entries[i].name);
+          fprintf(fptr, "struct Sequence %s;\n", table->entries[i].name);
           break;
         case TypeTuple:
-          fprintf(fptr, "struct TypeTuple *%s;\n", table->entries[i].name);
+          fprintf(fptr, "struct Sequence %s;\n", table->entries[i].name);
           break;
         case TypeSEQ:
-          fprintf(fptr, "struct TypeSEQ *%s;\n", table->entries[i].name);
+          fprintf(fptr, "struct Sequence %s;\n", table->entries[i].name);
           break;
         default:
           fclose(fptr);
@@ -1223,7 +1226,7 @@ void printparam(FILE *fptr, struct nodeType* node){
   case NODE_TYPE_SEQ:{
     switch(node->valueType){
       case TypeSEQ_I:
-        fprintf(fptr, "struct TypeSEQ_I*");
+        fprintf(fptr, "struct Sequence");
         break;
       default :
         //TODO
@@ -1245,7 +1248,7 @@ void APP3printFor(FILE *fptr, struct nodeType* node1, struct nodeType* node2){
   dumpTable(fptr, node2->child);
   
   // and initialize it by assigning an element value.
-  fprintf(fptr, "  %s = %s[i];\n",node1->child->child->string,seqname);
+  fprintf(fptr, "  %s = %s.ptr[i];\n",node1->child->child->string,seqname);
   
   // print the FILTER 
   // may also use boolean guard here.
@@ -1255,16 +1258,75 @@ void APP3printFor(FILE *fptr, struct nodeType* node1, struct nodeType* node2){
   codegen(fptr, node2->child);
   fprintf(fptr, "){\n");
   
-  fprintf(fptr, "    %s[count] = %s[i];\n    count++;\n  }\n", 
+  fprintf(fptr, "    %s.ptr[count] = %s.ptr[i];\n    count++;\n    %s.len++;\n}\n", 
           node1->parent->rsibling->child->string,
-          seqname);
+          seqname,
+          node1->parent->rsibling->child->string
+          );
   fprintf(fptr, "}\n");
   
 }
+
+void printNewSeq(FILE *fptr, struct nodeType* node){
+  
+  // this should be called from APP3printFor
+  // aim to allocate the tmp array v in quicksort.
+
+  assert(node->nodeType == NODE_IN);
+  struct nodeType *elm = node->child;
+  struct nodeType *newSEQ = node->child->rsibling;
+  //fprintf(fptr, "for( int i =0;  i < %d; i++){\n", newSEQ->counts);
+  struct nodeType *newelm = newSEQ->child;
+  fprintf(fptr, "%s = malloc(sizeof(%s)*%d);\n",elm->string, 
+                                        newelm->string, newSEQ->counts);
+  for(int i =0;i<newSEQ->counts;i++){
+    fprintf(fptr, "%s.ptr[i] = %s;\n",elm->string,newelm->string);
+    newelm = newelm->rsibling;
+  }
+
+}
+
 void APP2printFor(FILE *fptr, struct nodeType* node1, struct nodeType* node2){
   //char[100] seqname = node2->child->child->
-  fprintf(fptr, "for(int i = 0, count =0; i<newseq.len; i++){\n");
   
+  // below for qs.nesl
+  struct nodeType *newseq = node2->child->child->rsibling;
+  if(newseq!=0 && newseq->nodeType == NODE_NEW_SEQ){
+    // node1 might be functioncall or node_op;
+    // here consider node_op firstly.
+    fprintf(fptr, "{\n");
+    dumpTable(fptr, node1);
+    printNewSeq(fptr, node2->child);  
+    //fprintf(fptr, "for( int i =0; i<%d;i++){\n", newseq->counts);
+    //fprintf(fptr, "  %s.ptr[i] = %s;\n"
+    
+    switch(node1->nodeType){
+      case NODE_FUNC_CALL:{
+        struct nodeType *funcName = node1->child;
+        struct nodeType *funcPara = funcName->rsibling;// usually a node pair.
+        fprintf(fptr, "for( int i =0; i<%d;i++){\n", newseq->counts);
+        fprintf(fptr, "  %s.ptr[i] = %s(%s.ptr[i]);\n", 
+                node1->parent->rsibling->child->string,
+                funcName->string,
+                funcPara->child->string
+                );
+
+        fprintf(fptr, "}\n");
+
+        break;
+      }
+      default:
+        assert(0);// not implement.
+        break; 
+    }
+    fprintf(fptr, "}\n");
+
+  }else{ 
+    // other cases 
+
+    fprintf(fptr, "for(int i = 0, count =0; i<newseq.len; i++){\n");
+    fprintf(fptr, "}\n");
+  } 
   //fprintf(fptr, "for(int i = 0, count =0; i<%s.len; i++){\n");
 }
 
@@ -1311,22 +1373,25 @@ void codegen(FILE *fptr, struct nodeType* node){
               fprintf(fptr, "\n");
               break;
             case TypeSEQ_I:
-              fprintf(fptr, "struct TypeSEQ_I * %s", node->string);
+              fprintf(fptr, "struct Sequence  %s", node->string);
               //codegen(fptr,node->child);
               printparam(fptr, node->child);
-              fprintf(fptr, "{\nstruct TypeSEQ_I *res;\n");
+              fprintf(fptr, "{\nstruct Sequence res;\n");
+              fprintf(fptr, "struct Sequence tmp;\n");
               
               // FIXME how to dump table without the inputparam
               //dumpTable(fptr, node->child);
               
-              codegen(fptr,node->child->rsibling->rsibling);
+              codegen(fptr, node->child->rsibling->rsibling);
+              fprintf(fptr, "res = tmp;\nreturn res;\n");
               fprintf(fptr, "\n}\n");
               break;
             case TypeTuple:
-              fprintf(fptr, "struct TypeTuple * %s", node->string);
+              fprintf(fptr, "struct TypeTuple %s", node->string);
               codegen(fptr,node->child);
               fprintf(fptr, "{\n");
-              fprintf(fptr, "{\nstruct TypeTuple *res;\n");
+              fprintf(fptr, "{\nstruct TypeTuple res;\n");
+              fprintf(fptr, "struct TypeTuple tmp;\n");
               codegen(fptr,node->child->rsibling->rsibling);
               fprintf(fptr, "\n}\n");
               break;
@@ -1354,20 +1419,21 @@ void codegen(FILE *fptr, struct nodeType* node){
         
         case NODE_THENSTMT:
           fprintf(fptr, "{\n");
+          fprintf(fptr, "tmp = ");
           codegen(fptr, node->child);
-          fprintf(fptr, "\nreturn res;\n}");
+          fprintf(fptr, ";\n}");
           break;
 
         case NODE_ELSESTMT:
           fprintf(fptr, "else{\n");
           codegen(fptr, node->child);
-          fprintf(fptr, "\nreturn res;\n");
+          fprintf(fptr, "}\n");
           break;
         
         case NODE_LET:
           dumpTable(fptr, node);
           codegen(fptr, node->child);
-          fprintf(fptr, "\nres = ");
+          fprintf(fptr, "\ntmp = ");
           codegen(fptr, node->child->rsibling);
           fprintf(fptr, ";\n");
           break;
@@ -1394,34 +1460,48 @@ void codegen(FILE *fptr, struct nodeType* node){
               fprintf(fptr, " < ");
               codegen(fptr, node->child->rsibling);
               break;
+            
             case OP_GT:
               codegen(fptr, node->child);
               fprintf(fptr, " > ");
               codegen(fptr, node->child->rsibling);
               break;
+            
             case OP_EQ:
               codegen(fptr, node->child);
               fprintf(fptr, " == ");
               codegen(fptr, node->child->rsibling);
               break;
+            
             case OP_SHARP:
               codegen(fptr, node->child);
               fprintf(fptr, ".len");
               break;
+            
             case OP_BIND:
+              
               codegen(fptr, node->child);
-              fprintf(fptr, " = ");
-              codegen(fptr, node->child->rsibling);
-              if(node->child->rsibling->nodeType != NODE_APPLYBODY1 &&
-                 node->child->rsibling->nodeType != NODE_APPLYBODY2 &&
-                 node->child->rsibling->nodeType != NODE_APPLYBODY3 &&
-                 node->child->rsibling->nodeType != NODE_APPLYBODY4)
-                 fprintf(fptr, ";\n");
+              switch(node->child->rsibling->nodeType){
+              case NODE_APPLYBODY1:
+              case NODE_APPLYBODY2:
+              case NODE_APPLYBODY3:
+              case NODE_APPLYBODY4:
+                fprintf(fptr, ".ptr = ");
+                codegen(fptr, node->child->rsibling);
+                break;
+              default :
+                fprintf(fptr, " = ");
+                codegen(fptr, node->child->rsibling);
+                fprintf(fptr, ";\n");
+              }
               break;
+            
             case OP_PP:
+              fprintf(fptr, "concat(");
               codegen(fptr, node->child);
-              fprintf(fptr, "++");
+              fprintf(fptr, ",");
               codegen(fptr, node->child->rsibling);
+              fprintf(fptr, ")");
               break;
             case OP_DIV:
               codegen(fptr, node->child);
@@ -1433,7 +1513,7 @@ void codegen(FILE *fptr, struct nodeType* node){
        
         case NODE_SEQ_REF:
           codegen(fptr, node->child);
-          fprintf(fptr, "[");
+          fprintf(fptr, ".ptr[");
           codegen(fptr, node->child->rsibling);
           fprintf(fptr, "]");
           break;
@@ -1474,19 +1554,23 @@ void codegen(FILE *fptr, struct nodeType* node){
             assert(entry);
             switch(entry->type){
               case TypeSEQ_I:
-                fprintf(fptr, "malloc(sizeof(struct TypeSEQ_I));\n");
+                fprintf(fptr, "malloc(sizeof(int)*%s.len);\n",entry->name);
                 break;
               case TypeSEQ_F:
-                fprintf(fptr, "malloc(sizeof(struct TypeSEQ_F));\n");
+                fprintf(fptr, "malloc(sizeof(float)*%s.len);\n",entry->name);
                 break;
               case TypeSEQ_B:
-                fprintf(fptr, "malloc(sizeof(struct TypeSEQ_B));\n");
+                fprintf(fptr, "malloc(sizeof(bool)*%s.len);\n",entry->name);
                 break;
               case TypeSEQ_C:
-                fprintf(fptr, "malloc(sizeof(struct TypeSEQ_C));\n");
+                fprintf(fptr, "malloc(sizeof(char)*%s.len);\n",entry->name);
                 break;
               case TypeSEQ:
-                fprintf(fptr, "malloc(sizeof(struct TypeSEQ));\n");
+                fprintf(fptr, "malloc(sizeof(%s.ptr[0])*%s.len);\n",
+                                            entry->name,entry->name);
+                break;
+              default:
+                assert(0); //break;
                 break;
             }
             break;
@@ -1495,7 +1579,13 @@ void codegen(FILE *fptr, struct nodeType* node){
           case NODE_NEW_SEQ:
             switch(node->valueType){
               case TypeSEQ:
-                fprintf(fptr, "SEQ\n");
+                fprintf(fptr, "malloc(sizeof(%s)*%d);\n",
+                              node->child->rsibling->child->string,
+                              node->child->rsibling->counts);
+                //FIXME problems in the future.
+                fprintf(fptr, "%s.len = %d;\n",
+                              node->parent->parent->rsibling->child->string,
+                              node->child->rsibling->counts);
                 break;
 
             }
