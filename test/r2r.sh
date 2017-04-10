1acc19cac0e0290b5bde07d9eaf87ccbc434111e
jerrychen210054 <jerrychen210054@gmail.com>
2017-04-10 23:40:38 +0800

    [jerry] add a lot of testcases.

---
+++ test/r2r.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp qs_run_range1000.nesl       qs_run_1000.nesl    
+cp qs_run_range100000.nesl     qs_run_100000.nesl  
+cp qs_run_range1000000.nesl    qs_run_1000000.nesl 
+cp qs_run_range1500000.nesl    qs_run_1500000.nesl 
+cp qs_run_range200000.nesl     qs_run_200000.nesl  
+cp qs_run_range2000000.nesl    qs_run_2000000.nesl 
+cp qs_run_range2500000.nesl    qs_run_2500000.nesl 
+cp qs_run_range300000.nesl     qs_run_300000.nesl  
+cp qs_run_range3000000.nesl    qs_run_3000000.nesl 
+cp qs_run_range3500000.nesl    qs_run_3500000.nesl 
+cp qs_run_range3527314.nesl    qs_run_3527314.nesl 
+cp qs_run_range400000.nesl     qs_run_400000.nesl  
+cp qs_run_range500000.nesl     qs_run_500000.nesl  
+cp qs_run_range600000.nesl     qs_run_600000.nesl  
+cp qs_run_range700000.nesl     qs_run_700000.nesl  
+cp qs_run_range800000.nesl     qs_run_800000.nesl     
+cp qs_run_range900000.nesl     qs_run_900000.nesl
