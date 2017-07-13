6f10edeee6ca2e4e6de8f08741b5a5d6c6f5e568
tclin <tclin914@gmail.com>
2017-07-13 15:54:16 +0800

    add Visitor

---
+++ Visitor.h
@@ -0,0 +1,54 @@
+#ifndef NESL2C_VISITOR_H
+#define NESL2C_VISITOR_H
+
+namespace nesl2c {
+
+class Or;
+class NotOr;
+class XOr;
+class And;
+class NotAnd;
+class Add;
+class Subtract;
+class PP;
+class LARROW;
+class Mul;
+class Div;
+class RARROW;
+class Identifier;
+class TypeNode;
+class ConstantInteger;
+class ConstantFloat;
+class ConstantBoolean;
+class ConstantString;
+
+class Visitor {
+
+public:
+
+  virtual void Visit(Or*) = 0;
+  virtual void Visit(NotOr*) = 0;
+  virtual void Visit(XOr*) = 0;
+  virtual void Visit(And*) = 0;
+  virtual void Visit(NotAnd*) = 0;
+
+  virtual void Visit(Add*) = 0;
+  virtual void Visit(Subtract*) = 0;
+  virtual void Visit(PP*) = 0;
+  virtual void Visit(LARROW*) = 0;
+  virtual void Visit(Mul*) = 0;
+  virtual void Visit(Div*) = 0;
+  virtual void Visit(RARROW*) = 0;
+
+  virtual void Visit(Identifier*) = 0;
+  virtual void Visit(TypeNode*) = 0;
+  virtual void Visit(ConstantInteger*) = 0;
+  virtual void Visit(ConstantFloat*) = 0;
+  virtual void Visit(ConstantBoolean*) = 0;
+  virtual void Visit(ConstantString*) = 0;
+
+};
+
+} // namespace of nesl2c
+
+#endif
